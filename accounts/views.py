0769043293977be432c59bfb6684a9226942c385
SuharshaHarish <suharsha.gohan@gmail.com>
2021-06-13 00:47:33 +0530

    SMTP login

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.views.generic import View
 from django.contrib import messages
 # for email registration
-import smptlib
+import smtplib
 
 from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
