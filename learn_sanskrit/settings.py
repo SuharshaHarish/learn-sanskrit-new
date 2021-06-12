1026a366303ee56c2bc181c47322be10ec1035f5
SuharshaHarish <suharsha.gohan@gmail.com>
2021-06-12 22:27:03 +0530

    Database deployment

--- learn_sanskrit/settings.py
+++ learn_sanskrit/settings.py
@@ -94,6 +94,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Password validation
